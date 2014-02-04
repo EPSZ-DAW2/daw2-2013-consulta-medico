459c822090dc46890e674dc6e7ca1f310115c08e
toxokssk <toxokssk@gmail.com>
2014-02-04 19:30:24 +0000

    cambios realizados en plantillas, pruebas, facturas por Tomas aldonza raquel y pablo

--- protected/controllers/FacturasController.php
+++ protected/controllers/FacturasController.php
@@ -192,13 +192,23 @@ class FacturasController extends Controller
 			Yii::app()->end();
 		}
 	}/*PARA EL PDF*/
-	public function actionPdf($idpaciente,$idfactura,$serie,$numero,$fecha,$concepto,$importe,$fechac,$notas)
+	public function actionPdf($id)
     {
-		
-		$datos=array($idpaciente,$idfactura,$serie,$numero,$fecha,$concepto,$importe,$fechac,$notas);
-		//plantilla::generarplantilla($plantilla, array($model->IdFacturas,$model->Series,$model->Numero,$model->Fecha,$model->IdPaciente,$model->IdFacturas,$model->Concepto,$model->Importe,$model->FechaCobro,$model->Notas));
-		plantilla::generarplantilla('./Plantillas/PlantillaFactura', $datos);
-	
+		$factura= Facturas::model()->findByPk( $id);
+		if ($factura !== null) {
+			$datos=array(
+				'factura.idfactura' =>$factura->IdFactura,
+				'factura.fecha' =>$factura->Fecha,
+				'factura.idpaciente'=>$factura->IdPaciente,
+				'pacientes.nombre'=>CHtml::value( $factura, 'paciente.Nombre'),
+			);
+				//$serie,$numero,$fecha,$concepto,$importe,$fechac,$notas,$Plantillas);
+			//plantilla::generarplantilla($plantilla, array($model->IdFacturas,$model->Series,$model->Numero,$model->Fecha,$model->IdPaciente,$model->IdFacturas,$model->Concepto,$model->Importe,$model->FechaCobro,$model->Notas));
+			//plantilla::generarPlantilla( 'PlantillaFactura', $datos, false);
+			plantilla::generarPlantilla( 'PlantillaFactura', $datos, true);
+		} else {
+			echo 'Factura no encontrada.';
+		}
 	/*
 	$html="La factura con ID: " . $idfactura."\n\r Numero de Serie: ". $serie . "\n Numero de Factura: ". $numero . "\n Del paciente con ID: ". $idpaciente . "\n Concepto: " . $concepto . "\n Importe de Factura: " . $importe . "\n Notas de Factura: " . $notas;
 	
---
+++ protected/plantillas/Holter ECG/Holter/target.js
@@ -0,0 +1,20 @@
+	function load_canv_1(){
+		var canvas = document.getElementById("_canv_1");
+
+		if (canvas && canvas.getContext){
+			var ctx = canvas.getContext("2d");
+			ctx.lineWidth = .93333332;
+			ctx.lineCap = 'butt';
+			ctx.lineJoin = 'miter';
+			ctx.miterLimit = 13.3333;
+			ctx.strokeStyle = "rgba(0,0,0,1)";
+			ctx.beginPath();
+			ctx.moveTo(.93332926,.9332784);
+			ctx.lineTo(291.73,.9332784);
+			ctx.stroke();
+		}
+	}
+	
+function DrawPage1(){
+	load_canv_1();
+}
